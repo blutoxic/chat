09c4d0cb76d0949565de755e08006d1a0e12150c
Luca Herzog <herzog.luca@gmail.com>
2012-04-24 12:21:53 +0200

    Die Uebermittlung von Daten zwischen dem Server und den Clients via Faye wurde gewährleistet.

--- vendor/assets/javascripts/chat.js
+++ vendor/assets/javascripts/chat.js
@@ -1,1 +1,9 @@
-$(document).ready(function() {alert("Hello World");});
+public_session_user="new";
+$(document).ready(function() {
+  var faye = new Faye.Client('http://localhost:9292/faye');
+  var subscriptions = [faye.subscribe('/messages/'+public_session_user, function(data) {
+        alert(data);
+   })];
+     
+   //subscriptions.push(faye.subscribe('/messages/public', function (data) {eval(data); }));
+});
