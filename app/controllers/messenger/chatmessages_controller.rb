d0b0b407eefd53788aa52a022450725c354d971c
Luca Herzog <herzog.luca@gmail.com>
2012-04-30 16:40:09 +0200

    Chatten ist nun erstmals möglich User können sich Nachrichten schicken. Controller-Logik und Views wurde erweitert und angepasst. Erste CSS-Elemente wurden implementiert.

--- app/controllers/messenger/chatmessages_controller.rb
+++ app/controllers/messenger/chatmessages_controller.rb
@@ -1,45 +1,54 @@
 class Messenger::ChatmessagesController < ApplicationController
   def index
-      @current_user=User.current
+      @current_chat_user=User.current
 
-      @chat_partners=Messenger::Openchat.find(:all,:conditions => "creator_id = '#{@current_user.id}'")
-      
-      @chat_messages = Array.new(2, Hash.new)
-      
-      
-      
-      
-      @chat_partners.each do |chat_partner| 
-  				if chat_partner.chat_partner_id == 1 then
-  				  @message_data_public=Chatmessage.find(:all,:conditions => "receiver_id = 1")
+      @chat_partners=Messenger::Openchat.find(:all,:conditions => "creator_id = '#{@current_chat_user.id}'")
 
-  				else 
-  				  @message_data = Chatmessage.find(:all,:conditions => "creator_id='"+chat_partner.chat_partner_id+"' and receiver_id = '" + 
-  				  User.current.id + "' or username='"+User.current.id+"' and receiver_id = '" + chat_partner.chat_partner_id + "'")
-  				end
-  				
-  				if chat_partner.chat_partner_id != User.current.id then
-  				  @message_data.each do |message|
-  				    @chat_messages[message][chat_partner.chat_partner_id]=@message_data.content
-				    end
-  				  #render :partial => "chats/chat_form", :locals=>{:channel_id=>chat_partner.id, :channel=>chat_partner.open_chat_user,:messages=>@message_data} 
-  				end
-  		end
-=begin
+      @latest_chat_partner=Chatmessage.find(:last,:conditions => "creator_id='#{@current_chat_user.id}'").receiver_id
+
       public_entry=false
     	@chat_partners.each do |chat_partner|
-    	  if chat_partner.chat_partner_id=="public"
+    	  if chat_partner.chat_partner_id==0
     	    public_entry=true
     	    break
     	  end
     	end
     	if public_entry==false
-    	  @open_chat=Openchat.new
-        @open_chat.user=@current_user.login
-        @open_chat.chat_partner_id="public"
+    	  @open_chat=Messenger::Openchat.new
+        @open_chat.creator_id=@current_chat_user
+        @open_chat.chat_partner_id=0
         @open_chat.save
         @chat_partners.push(@open_chat)
     	end
-=end
+
+      @chat_messages = Array.new(2, Hash.new)
+  
+      
+      @chat_partners.each do |chat_partner| 
+
+  		@message_data=Chatmessage.find(:all,:conditions => "creator_id='#{chat_partner.chat_partner_id}' and receiver_id = '
+  		#{@current_chat_user.id}' or creator_id='#{@current_chat_user.id}' and receiver_id = '#{chat_partner.chat_partner_id}'")
+
+  			#	if chat_partner.chat_partner_id != @current_chat_user.id then
+  			#	  if @message_data!=nil
+  			#	    @message_data.each do |message|
+  			#	      @chat_messages[message][chat_partner.chat_partner_id]=@message_data.content
+				 #     end
+			  #    end
+  			#	end
+  		end
+  		
+  	
   end
+  
+  def create
+      if params[:create][:content]!="" 
+        #Essential for notifications:
+        @r_old_channel = Messenger::Openchat.find(:last)
+        params[:create][:creator_id]=User.current.id
+        @message = Chatmessage.create!(params[:create])
+      end
+  end
+  
+  
 end
